c8a36c7cb5c3d4b2df8a6ea62ca51d11d68814aa
wdorji <wangdrak.dorji@gmail.com>
2024-02-01 21:47:37 -0500

    Update [...nextauth].ts

--- amplify/src/pages/api/auth/[...nextauth].ts
+++ amplify/src/pages/api/auth/[...nextauth].ts
@@ -7,11 +7,7 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_ID!,
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET!,
-      // clientId:
-      //   "261857999407-qkgt02ljrft2kdfn6h5po96g48igihsu.apps.googleusercontent.com",
-      // clientSecret: "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",
     }),
-    // ...add more providers here
   ],
 };
 export default NextAuth(authOptions);
